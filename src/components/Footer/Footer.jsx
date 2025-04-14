c756e1eb34395b8ae24bc0d608a8cd6d1dbd151f
Sopuruoti641 <sopuruoti641@gmail.com>
2025-04-13 23:58:56 -0700

    changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__description">
+        Developed by Name Sopuru <span id="footer-year">2025</span>
+      </p>
+    </footer>
+  );
+}
+export default Footer;
